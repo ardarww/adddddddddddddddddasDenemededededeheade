24240bbba9e55f5f86a9df300422f99b51b70893
ardarww <79516457+ardarww@users.noreply.github.com>
2023-11-22 21:58:54 +0300

    dedededede
    dedeeddeafsd

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    data: {
+        guildId: "", // Server ID
+    },
+    bot: {
+        id: "", // Bot ID
+        token: "", // Bot token
+        secret: "", // Bot secret
+
+        status: "online", // Bot status
+        statusName: "test 1 2 3", // Bot status name
+    },
+    web: {
+        url: "http://localhost:3001", // Do not change - ELLEME
+        port: 3001, // Do not change - ELLEME
+    } 
+}
+
+/*
+https://discord.gg/dcsmm - Developer: Kadrxy | https://discord.gg/dctoken - By. Endy
+https://discord.gg/dcsmm - Developer: Kadrxy | https://discord.gg/dctoken - By. Endy
+https://discord.gg/dcsmm - Developer: Kadrxy | https://discord.gg/dctoken - By. Endy
+*/
